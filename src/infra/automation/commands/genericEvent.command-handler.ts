cc4fe110965dedfde36aab7880e963fc1d280700
henrique weiand <henriqueweiand@gmail.com>
2024-03-30 17:02:36 -0300

    wip

--- src/infra/automation/commands/genericEvent.command-handler.ts
+++ src/infra/automation/commands/genericEvent.command-handler.ts
@@ -17,7 +17,10 @@ export class GenericEventCommandHandler
         const actionModuleRef = await this.lazyModuleLoader.load(() =>
             import('../actions/actions.module').then((m) => m.ActionsModule),
         );
-        //  improve organization and segmentation of folders to be to be able to have modules for a better danimyc code 
+
+        // improve organization and segmentation of folders to be to be able to have modules for a better danimyc code 
+        // how can i handle errors here?
+        // should repository implement transactions?
 
         const actionModule = await import(`../actions/${command.event}`);
         const ActionClass = actionModule.default;
